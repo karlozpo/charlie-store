83118024e046b3fe5a9dffc8a19ac399212d4ba5
Carlos Posada <carlosposadat@gmail.com>
2021-07-16 18:41:11 -0500

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './spin.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Mi tienda online con react js
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprendiendo React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
